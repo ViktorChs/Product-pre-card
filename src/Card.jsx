8eefe01774140b69dfcc80226cc470206b9ff25b
ViktorChs <Victoraltas123@gmail.com>
2023-08-04 12:40:03 -0400

    ready

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,6 @@ export function Card() {
 <div className="Main_img-container">
 <img src="./src/images/image-product-mobile.jpg" alt="essence" />
 <img src="./src/images/image-product-desktop.jpg" alt="essence" />
-
 </div>
 <div className="Main_text-container">
 <p><span>PERFUME</span></p>
@@ -12,8 +11,8 @@ export function Card() {
 <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, 
   Perfumer-Creator for the House of CHANEL.</p>
   <div className="Main_price-container">
-  <p>$149.99</p>
-  <p>$169.99</p>
+  <s>$149.99</s>
+  <s>$169.99</s>
   </div>
   
   <button className="Bts_add-to-cart"><img src="./src/images/icon-cart.svg" alt="cart icont"/><strong>Add to Cart</strong></button>
